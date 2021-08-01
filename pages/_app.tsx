3c874c92c1625ab85c836da1538e260661668887
Frederico Batista <rincofrederico@gmail.com>
2021-08-01 20:05:37 +0100

    feat: implement chakra-ui

--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import { AppComponent } from 'next/dist/next-server/lib/router/router'
-
-const App: AppComponent = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-export default App
